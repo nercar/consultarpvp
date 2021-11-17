74246fd4568fb61990a46dd28b9224aa56389ec9
nercar <nercar@gmail.com>
2021-11-17 10:04:53 -0400

    Respaldo Desarrollo

---
+++ DBProcs.php
@@ -0,0 +1,150 @@
+﻿<?php
+	/**
+	* Permite obtener los datos de la base de datos y retornarlos
+	* en modo json o array
+	*/
+	try {
+		date_default_timezone_set('America/Caracas');
+		// Se capturan las opciones por Post
+		$opcion = (isset($_POST["opcion"])) ? $_POST["opcion"] : "";
+		// id para los filtros en las consultas
+		$idpara = (isset($_POST["idpara"])) ? $_POST["idpara"] : '';
+		$fecha  = date("DD-MM-YYYY");
+		$hora   = date("hh:i");
+		// Se establece la conexion con la BBDD
+		$params = parse_ini_file('dist/config.ini');
+		if ($params === false) {
+			throw new \Exception("Error reading database configuration file");
+		}
+		// connect to the sql server database
+		if($params['instance']!='') {
+			$conStr = sprintf("sqlsrv:Server=%s\%s;",
+			$params['host_sql'],
+			$params['instance']);
+		} else {	
+			$conStr = sprintf("sqlsrv:Server=%s,%d;",		
+			$params['host_sql'],		
+			$params['port_sql']);
+		}
+		$connec = new \PDO($conStr, $params['user_sql'], $params['password_sql']);
+		$moneda = $params['moneda'];
+		$simpvp1 = 'Ref.$';
+		$simpvp2 = 'Bs.';
+		$simpvp3 = 'COP';
+		$vercop  = $params['vercop'];
+		switch ($opcion) {
+			case 'hora_srv':
+				echo json_encode('1¬' . $hora);
+				break;
+			case 'consultarpvp':
+				$sql = "SELECT EA.codigo, EC.barra, EA.descripcion AS nombre, ED.DESCRIPCION AS dpto,
+							EA.precio1 AS base, EA.impuesto, EA.moneda,
+							(SELECT FACTOR FROM BDES.dbo.ESFormasPago_FactorC WHERE CODIGO = 60) AS tasa,
+							(SELECT FACTOR FROM BDES.dbo.ESFormasPago_FactorC WHERE CODIGO = 61) AS cops,
+							(CASE WHEN CAST(EA.fechainicio AS TIME) != '00:00:00' OR CAST(EA.fechafinal AS TIME) != '00:00:00' THEN
+								(CASE WHEN GETDATE() BETWEEN EA.fechainicio AND EA.fechafinal
+									THEN EA.preciooferta ELSE 0 END)
+							ELSE
+								(CASE WHEN CAST(GETDATE() AS DATE) 
+									BETWEEN CAST(EA.fechainicio AS DATE) AND CAST(EA.fechafinal AS DATE)
+									THEN EA.preciooferta ELSE 0 END)
+							END) AS oferta
+						FROM BDES.dbo.ESARTICULOS EA
+							INNER JOIN BDES.dbo.ESCodigos EC ON EC.escodigo = EA.codigo
+							INNER JOIN BDES.dbo.ESDpto ED ON ED.CODIGO = EA.departamento
+						WHERE EC.barra = '$idpara'";
+				// Se ejecuta la consulta en la BBDD
+				$sql = $connec->query($sql);
+				$datos = [];
+				if($sql) {
+					$row = $sql->fetch();
+					if($row) {
+						if($vercop==1) {
+							$stypvp1 = 'font-size: 650%; font-weight: bold; letter-spacing: -5px;  line-height: 0.8em;';
+							$stypvp2 = 'font-size: 450%; font-weight: bold; letter-spacing: -10px; line-height: 0.8em;';
+							$stypvp3 = 'font-size: 550%; font-weight: bold; letter-spacing: -12px;  line-height: 0.8em;';
+						} else {
+							$stypvp1 = 'font-size: 700%; font-weight: bold; letter-spacing: -5px;  line-height: 0.8em;';
+							$stypvp2 = 'font-size: 550%; font-weight: bold; letter-spacing: -10px; line-height: 0.8em;';
+							$stypvp3 = 'font-size: 100%; font-weight: bold; letter-spacing: -12px;  line-height: 0.8em;';
+						}
+						$stysimb = 'font-size: 35%; letter-spacing: -2px;';
+						$style   = "color: black; width: fit-content; text-align: center; ".
+									"text-shadow: ". 
+										"4px  -0   0.5px yellow, -4px -0   0.5px yellow,".
+										"4px  -1px 0.5px yellow, -4px -1px 0.5px yellow,".
+										"4px  -2px 0.5px yellow, -4px -2px 0.5px yellow,".
+										"4px  -3px 0.5px yellow, -4px -3px 0.5px yellow,".
+										"4px  -4px 0.5px yellow, -4px -4px 0.5px yellow,".
+										"-4px  0   0.5px yellow,  0    0   0.5px yellow,".
+										"-4px  1px 0.5px yellow,  0    1px 0.5px yellow,".
+										"-4px  2px 0.5px yellow,  0    2px 0.5px yellow,".
+										"-4px  3px 0.5px yellow,  0    3px 0.5px yellow,".
+										"-4px  4px 0.5px yellow,  0    4px 0.5px yellow,".
+										"0    -0   0.5px yellow,  4px  0   0.5px yellow,".
+										"0    -1px 0.5px yellow,  4px  1px 0.5px yellow,".
+										"0    -2px 0.5px yellow,  4px  2px 0.5px yellow,".
+										"0    -3px 0.5px yellow,  4px  3px 0.5px yellow,".
+										"0    -4px 0.5px yellow,  4px  4px 0.5px yellow;";
+						if($row['moneda']==0) {
+							$precio2 = round($row['base']   * ( 1 + ( $row['impuesto'] / 100 ) ), 2);
+							$oferta2 = round($row['oferta'] * ( 1 + ( $row['impuesto'] / 100 ) ), 2);
+							$precio1 = $precio2 / $row['tasa'];
+							$oferta1 = $oferta2 / $row['tasa'];
+							$precio3 = $precio2 * $row['cops'];
+							$oferta3 = $oferta2 * $row['cops'];
+							$pvpbsdi = round($precio2 * 1000000, 2);
+							$offbsdi = round($oferta2 * 1000000, 2);
+						} else {
+							$precio1 = round($row['base']   * ( 1 + ( $row['impuesto'] / 100 ) ), 2);
+							$oferta1 = round($row['oferta'] * ( 1 + ( $row['impuesto'] / 100 ) ), 2);
+							$precio2 = $precio1 * $row['tasa'];
+							$oferta2 = $oferta1 * $row['tasa'];
+							$precio3 = $precio1 * $row['tasa'] * $row['cops'];
+							$oferta3 = $oferta1 * $row['tasa'] * $row['cops'];
+							$pvpbsdi = round($precio2 * 1000000, 2);
+							$offbsdi = round($oferta2 * 1000000, 2);
+						}
+						$oferta  = $oferta1;
+						$precio1 = '<span style="'.$stypvp1.'"><span style="font-size: 25%; letter-spacing: -2px;">'.$simpvp1 . '</span>' . number_format($precio1, 2).'</span>';
+						$oferta1 = '<span style="'.$stypvp1.'"><span style="'.$style.'"><span style="font-size: 25%; letter-spacing: -2px;">'.$simpvp1 . '.</span>' . number_format($oferta1, 2) . '</span></span>';
+						$precio2 = '<span style="'.$stypvp2.'"><span style="'.$stysimb.'">'.$simpvp2.'&nbsp;</span>' . number_format(intval($precio2), 0).'.<small>'.str_pad(number_format(($precio2-intval($precio2))*100, 0), 2, '0', STR_PAD_LEFT).'</small></span>';
+						$precio2.= '<br><span style="'.$stypvp2.'"><span style="'.$stysimb.'">Bs.S&nbsp;</span>' . number_format(intval($pvpbsdi), 0).'.<small>'.str_pad(number_format(($pvpbsdi-intval($pvpbsdi))*100, 0), 2, '0', STR_PAD_LEFT).'</small></span>';
+						$oferta2 = '<span style="'.$stypvp2.'"><span style="'.$style.'"><span style="'.$stysimb.'">'.$simpvp2.'&nbsp;</span>' . number_format(intval($oferta2), 0).'.<small>'.str_pad(number_format(($oferta2-intval($oferta2))*100, 0), 2, '0', STR_PAD_LEFT).'</small></span></span>';
+						$oferta2.= '<br><span style="'.$stypvp2.'"><span style="'.$style.'"><span style="'.$stysimb.'">Bs.S&nbsp;</span>' . number_format(intval($offbsdi), 0).'.<small>'.str_pad(number_format(($offbsdi-intval($offbsdi))*100, 0), 2, '0', STR_PAD_LEFT).'</small></span></span>';
+						if($vercop==1) {
+							$precio3 = '<span style="'.$stypvp3.'"><span style="'.$stysimb.'">'.$simpvp3.'&nbsp;</span>' . number_format(intval($precio3), 0).'.<small>'.number_format(($precio3-intval($precio3))*100, 0).'</small></span>';
+							$oferta3 = '<span style="'.$stypvp3.'"><span style="'.$style.'"><span style="'.$stysimb.'">'.$simpvp3.'&nbsp;</span>' . number_format(intval($oferta3), 0).'.<small>'.str_pad(number_format(($oferta3-intval($oferta3))*100, 0), 2, '0', STR_PAD_LEFT).'</small></span></span>';
+						} else {
+							$precio3 = '';
+							$oferta3 = '';
+						}
+						$datos[] = [
+							'codigo'  => $row['codigo'],
+							'barra'   => $row['barra'],
+							'nombre'  => $row['nombre'],
+							'dpto'    => $row['dpto'],
+							'precio1' => $precio1,
+							'oferta1' => $oferta1,
+							'precio2' => $precio2,
+							'oferta2' => $oferta2,
+							'precio3' => $precio3,
+							'oferta3' => $oferta3,
+							'oferta'  => $oferta,
+						];
+					}
+				}
+				// Se retornan los datos obtenidos
+				echo json_encode($datos);
+				break;
+			default:
+				# code...
+				break;
+		}
+		// Se cierra la conexion
+		$connec = null;
+	} catch (Exception $e) {
+		echo "Error : " . $e->getMessage() . "<br/>";
+		die();
+	}
+?>
---
+++ index.php
@@ -0,0 +1,149 @@
+﻿<?php
+	$params = parse_ini_file('dist/config.ini');
+	if ($params === false) {
+		$titulo = '';
+	}
+	$titulo = $params['title'];
+	$version = $params['version'];
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title><?php echo $titulo; ?></title>
+		<!-- Tell the browser to be responsive to screen width -->
+		<meta name="viewport" content="width=device-width">
+		<meta name="mobile-web-app-capable" content="yes">
+		
+		<!-- Icon Favicon -->
+		<link rel="shortcut icon" href="dist/img/favicon.png">
+		<link rel="icon" sizes="192x192" href="dist/img/favicon.png">
+		
+		<!-- Theme style -->
+		<link rel="stylesheet" href="dist/css/adminlte.css">
+
+		<style>
+			body {
+				background: url(dist/img/fondo.jpeg);
+				background-repeat: no-repeat;
+				background-attachment: fixed;
+				background-position: top;
+				background-size: 100vw 100vh;
+			}
+
+			.thickOutlined {
+				color: white;
+				width: fit-content;
+				text-align: center;
+				text-shadow: -2px -2px 0 #000,
+							0px -2px 0 #000,
+							2px -2px 0 #000,
+							-2px  2px 0 #000,
+							0px  2px 0 #000,
+							2px  2px 0 #000;
+			}
+			
+			#nombre { font-size: 250%; line-height: 0.8em; font-weight: bold; }
+			#alerta { font-size: 350%; line-height: 0.8em; }
+		</style>
+	</head>
+	<body onload="$('#barra').focus()" onclick="$('#barra').focus()">
+		<center><span class="badge badge-info m-0"> <?php echo $version; ?> </span></center>
+		<div id="ppal" class="p-0 pt-4 m-0" onclick="$('#barra').focus()">
+			<input type="hidden" name="hora_act" id="hora_act" value="">
+			<div class="d-flex justify-content-center align-content-center" onclick="$('#barra').focus()">
+				<div style="width: 40vw;"></div>
+				<div style="height: 88vh; width: 60vw;">
+					<center><input type="text" id="barra" class="text-center bg-transparent"  onblur="$('#barra').focus()"></center>
+					<br>
+					<br>
+					<br>
+					<div class="w-100 text-right">
+						<div id="nombre" class="bg-transparent ml-5 mr-5 text-center"></div>
+					</div>
+					<div class="text-right" style="position: absolute; top: 35vh; right: 3vw;">
+						<div id="precio1" class="bg-transparent mr-5"></div>
+						<div id="precio2" class="bg-transparent mr-5"></div>
+						<div id="precio3" class="bg-transparent mr-5"></div>
+						<center><span id="alerta" class="thickOutlined text-danger bg-danger m-5 p-3 d-none rounded border border-dark elevation-4">!!Código No Existe!!</span></center>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+
+	<!-- jQuery -->
+	<script src="dist/js/jquery.min.js"></script>
+	<!-- jQuery UI 1.12.1 -->
+	<script src="dist/js/jquery-ui.min.js"></script>
+	<!-- Bootstrap 4 -->
+	<script src="dist/js/bootstrap.bundle.min.js"></script>
+	<!-- AdminLTE App -->
+	<script src="dist/js/adminlte.min.js"></script>
+	<!-- JS propias app -->
+	<script src="dist/js/app.js"></script>
+
+	<script>
+		var tiempo = '';
+
+		$("body").on("keydown", "input", function(e) {
+			// si presiono el enter
+			if (e.keyCode == 13) {
+				if($('#barra').val()!='') {
+					$('#alerta').addClass('d-none');
+					$('#precio1').removeClass('d-none');
+					$('#precio2').removeClass('d-none');
+					$('#precio3').removeClass('d-none');
+					clearTimeout(tiempo);
+					$.ajax({
+						data: {
+							opcion: "consultarpvp",
+							idpara: $('#barra').val()
+						},
+						type: "POST",
+						dataType: "json",
+						url: "DBProcs.php",
+						success: function (data) {
+							if(data.length > 0) {
+								$('#barra').val('');
+								$('#nombre').html(data[0]['nombre']);
+								if(data[0]['oferta']!=0) {
+									$('#precio1').html(data[0]['oferta1']);
+									$('#precio2').html(data[0]['oferta2']);
+									$('#precio3').html(data[0]['oferta3']);
+								} else {
+									$('#precio1').html(data[0]['precio1']);
+									$('#precio2').html(data[0]['precio2']);
+									$('#precio3').html(data[0]['precio3']);
+								}
+								tiempo = setTimeout("inicializar()", 15000);
+							} else {
+								$('#precio1').addClass('d-none');
+								$('#precio2').addClass('d-none');
+								$('#precio3').addClass('d-none');
+								$('#alerta').removeClass('d-none');
+								inicializar();
+								setTimeout(function() {
+									$('#alerta').addClass('d-none');
+									$('#precio1').removeClass('d-none');
+									$('#precio2').removeClass('d-none');
+									$('#precio3').removeClass('d-none');
+								}, 15000);
+							}
+						}
+					});
+				}
+				return false;
+			}
+		});
+
+		function inicializar() {
+			$('#barra').val('');
+			$('#nombre').html('&nbsp;');
+			$('#precio1').html('&nbsp;');
+			$('#precio2').html('&nbsp;');
+			$('#precio3').html('&nbsp;');
+		}
+	</script>	
+</html>
